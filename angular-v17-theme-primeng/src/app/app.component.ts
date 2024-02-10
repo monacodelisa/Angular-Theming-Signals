5388827d3bfcd103b875f84a586fa4a941b6b434
Esther White <monacodelisa@gmail.com>
2024-02-10 12:57:49 +0200

    prime ng icons

--- angular-v17-theme-primeng/src/app/app.component.ts
+++ angular-v17-theme-primeng/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- angular-v17-theme-primeng/src/app/components/header/header.component.ts
+++ angular-v17-theme-primeng/src/app/components/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ToolbarModule, ButtonModule, InputSwitchModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  checked: boolean = true;
 }
